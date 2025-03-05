19f9ecc600cf3691ae00f62bbe83e637f48b55eb
Jhonatan Keb <jhonykeb123@gmail.com>
2025-03-05 01:46:12 -0600

    Login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,10 +1,99 @@
+import 'package:delivery_app_jk_yc/components/my_button.dart';
+import 'package:delivery_app_jk_yc/components/my_textfield.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+class LoginPage extends StatefulWidget {
+  final void Function()? onTap;
+
+  const LoginPage({super.key, required this.onTap});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  // text editing controllers
+  final TextEditingController emailController = TextEditingController();
+
+  final TextEditingController passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // logo
+            Icon(
+              Icons.lock_open_rounded,
+              size: 100,
+              color: Theme.of(context).colorScheme.inversePrimary,
+            ),
+
+            const SizedBox(height: 25),
+
+            // messege, app slogan
+            Text(
+              "Food Delivery App",
+              style: TextStyle(
+                fontSize: 16,
+                color: Theme.of(context).colorScheme.inversePrimary,
+              ),
+            ),
+
+            const SizedBox(height: 25),
+
+            // email textfield
+            MyTextfield(
+              controller: emailController,
+              hintText: "Email",
+              obscureText: false,
+            ),
+
+            const SizedBox(height: 10),
+
+            // password textfield
+            MyTextfield(
+              controller: passwordController,
+              hintText: "Password",
+              obscureText: true,
+            ),
+
+            const SizedBox(height: 10),
+
+            // sign in button
+            MyButton(text: "Sign In", onTap: () {}),
+
+            const SizedBox(height: 25),
+
+            // not a remember? register now
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Not a member?",
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.inversePrimary,
+                  ),
+                ),
+                const SizedBox(width: 4),
+                GestureDetector(
+                  onTap: widget.onTap,
+                  child: Text(
+                    "Register now",
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.inversePrimary,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
