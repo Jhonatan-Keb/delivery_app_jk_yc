fb4d0e009a93bcbbd21fa41c07cba265f169d1f6
Jhonatan Keb <jhonykeb123@gmail.com>
2025-03-05 03:09:42 -0600

    Menu drawer

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:delivery_app_jk_yc/components/my_drawer.dart';
+import 'package:delivery_app_jk_yc/main.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,6 +12,6 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("Home")));
+    return Scaffold(appBar: AppBar(title: Text("Home")), drawer: MyDrawer());
   }
 }
