7780635077ce91f7b5f0279f653c9370178cb838
Jhonatan Keb <jhonykeb123@gmail.com>
2025-03-05 00:45:01 -0600

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:delivery_app_jk_yc/pages/login_page.dart';
+import 'package:delivery_app_jk_yc/themes/theme_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const LoginPage(),
+      theme: Provider.of<ThemeProvider>(context).themeData,
+    );
+  }
+}
